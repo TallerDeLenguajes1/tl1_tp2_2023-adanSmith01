9d46eb722e2f151f2a879fb39b91234dcd0a22f7
AdanSmith <smith.paredes.13@gmail.com>
2023-03-23 18:47:14 -0300

    vector de 20 numeros aleatorios

---
+++ tp2_1_1.c
@@ -0,0 +1,18 @@
+// codigo a completar
+//codigo a completar
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define N 20
+
+int main()
+{
+    int i;
+    double vt[N];
+    srand(time(NULL));
+    for (i = 0; i < N; i++) {
+        vt[i] = 1 + (rand() % 100);
+        printf("%f\n", vt[i]);
+    }
+    return 0;
+}
